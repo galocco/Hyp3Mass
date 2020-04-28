1c8ccb75477b61b4d1f53cf86a43d401dd583c4f
Giacomo Alocco <40990466+galocco@users.noreply.github.com>
2020-04-28 09:40:59 +0200

    bugs fixed

--- efficiency_analysis.cpp
+++ efficiency_analysis.cpp
@@ -12,12 +12,11 @@ void ordered_indexes(int ,int []);
 void selector_efficiencies(char * input_name = "selector_results.root",char * output_name = "efficiency.root",char* pdf_file = "efficiency_plots.pdf",char * folder_name = "plot_folder")
 {
   gStyle->SetOptStat(0);
-  gStyle->SetOptTitle(0);
+  gStyle->SetOptTitle(1);
   
   gSystem->Exec(Form("mkdir %s",folder_name));
 
   const char lAM[3]{"AM"};
-  const char *lPrintMode[3]={"[","","]"};
   const char *lProj[2]{"pT", "ct"};
   const char *lProjSet[2]{"zx", "zy"};
   const char *lVarName[3]={ "NsigmaTPC","NclusterTPC","NclusterITS"};
@@ -25,11 +24,11 @@ void selector_efficiencies(char * input_name = "selector_results.root",char * ou
   const char* lProjMeas[3] = {"Rec","Fake","Clones"};
 
   TFile input_file(input_name);
-  TFile output_file(output_name,"RECREATE");
+  TFile output_file(Form("%s/%s",folder_name,output_name),"RECREATE");
   const int ndir=5;
   TDirectory* subdir;
   for(int iVar=0; iVar<3; iVar++){
-    for(int iMeas=0; iMeas<4; iMeas++)
+    for(int iMeas=0; iMeas<3; iMeas++)
       for(int iProj=0; iProj<2; iProj++)
         subdir = output_file.mkdir(Form("%s/%s/%s",lVarName[iVar],lMeasure[iMeas],lProj[iProj]));
   }
@@ -40,7 +39,7 @@ void selector_efficiencies(char * input_name = "selector_results.root",char * ou
   TH2D* fHistGenTot = nullptr;
   TH1D* fHistGen[2] = {nullptr};//for pt and ct
   TCanvas cv;
-  cv.Print(Form("%s[",pdf_file));
+  cv.Print(Form("%s/%s",folder_name,pdf_file));
   //array for the indexes of the cuts, with the cuts ordered from the tighter to the looser
   int ordered_cuts[number_of_variations];
   //matter or antimatter
@@ -88,97 +87,118 @@ void selector_efficiencies(char * input_name = "selector_results.root",char * ou
           //plot of the histograms in a single canvas
           rainbow_plot(fHist[iMeas],cv,Form("RainbowPlot_%s_%s_%s_%c",lVarName[iVar],lMeasure[iMeas],lProj[iProj],lAM[iMat]),true);          
           output_file.cd();
-          cv.Write();
-          std::cout<<Form("RainbowPlot_%s_%s_%s_%c",lVarName[iVar],lMeasure[iMeas],lProj[iProj],lAM[iMat])<<" ";
-          
-          cv.Print(Form("%s",pdf_file)); 
+          cv.Write();          
+          cv.Print(Form("%s/%s",folder_name,pdf_file)); 
           cv.Clear();
         }
         //plot of the three measurements in a single canvas
         multirainbow_plot(fHist,cv,Form("MultiRainbowPlot_%s_%s_%c",lVarName[iVar],lProj[iProj],lAM[iMat]),true);
-        cv.Print(Form("%s",pdf_file));
+        cv.Print(Form("%s/%s",folder_name,pdf_file));
         cv.Clear();
       }
     }
   }
-  cv.Print(Form("%s]",pdf_file));
+  cv.Print(Form("%%s/s]",folder_name,pdf_file));
 }
-/*
-void compare_efficiency(char * std_name = "efficiency.root",char * kf_name = "efficiencyKF.root",char * output_name = "comparison.root")
+///
+void compare_efficiencies(char* Std_name="efficiencyStd.root", char* KF_name="efficiencyKF.root", char* O2_name="efficiency02.root", char* output_name="efficiency_comparison.root", char* pdf_file="efficiency_comparison.pdf", char* folder_name="nome")
 {
-  gSystem->Exec(Form("mkdir comparison_images"));
   gStyle->SetOptStat(0);
-  TFile std_file(std_name);
-  TFile kf_file(kf_name);
-  //TODO:  hardcoded names
-  TFile stdmass_file("selector_resultsStd.root");
-  TFile kfmass_file("selector_resultsKF.root");
+  gStyle->SetOptTitle(1);
+  
+  gSystem->Exec(Form("mkdir %s",folder_name));
 
   const char lAM[3]{"AM"};
   const char *lProj[2]{"pT", "ct"};
-  const char *lProjSet[2]{"zx", "zy"};
-  const char *lVarName[3]={"NsigmaTPC", "NclusterTPC","NclusterITS"};
-  const char *lMeasure[4]={"Efficiency","Resolution","FakeRate","CloneRate"};
-  //Gen must be the first of the list
-  const char* lProjMeas[5] = {"Gen","Rec","Res","Fake","Clones"};
-  const char *lType[3]{"true", "fake", "clones"};
+  const char *lMeasure[3]={"Efficiency","FakeRate","CloneRate"};
+  const char* lProjMeas[3] = {"Rec","Fake","Clones"};
+  const char* lTitle[3] = {"Standard vertexer","0^{2} vertexer","Kalman Filter"};
 
+  TFile* input_file[3];
+  input_file[0] = new TFile(Std_name);
+  input_file[1] = new TFile(O2_name);
+  input_file[2] = new TFile(KF_name);
   TFile output_file(output_name,"RECREATE");
-  output_file.cd();
+  TH1D* fHist[3][3];
 
-  TCanvas cv("","");
-
-  TH1D* fHistkf;
-  TH1D* fHiststd;
-  TH2D* fHistInvMass;
-  //matter or antimatter
-  for(int iMatter=0; iMatter<2; iMatter++){
-    output_file.cd();
-    for(int iType=0; iType<3; iType++){
-      fHistInvMass = (TH2D*) stdmass_file.Get(Form("fHistInvMassPt_%c_%s",lAM[iMatter],lType[iType]));
-      fHistInvMass->SetName(Form("STDfHistInvMassPt_%s_%c",lType[iType],lAM[iMatter]));
-      fHistInvMass->Write();
-      fHistInvMass = (TH2D*) kfmass_file.Get(Form("fHistInvMassPt_%c_%s",lAM[iMatter],lType[iType]));
-      fHistInvMass->SetName(Form("KFfHistInvMassPt_%s_%c",lType[iType],lAM[iMatter]));
-      fHistInvMass->Write();
-    }
-    TLine zeroline(0,0,0,1);
-    //projection on pt or ct
+  TCanvas cv;
+  cv.Print(Form("%s[",pdf_file));
+  for(int iMat=0; iMat<2; iMat++){
     for(int iProj=0; iProj<2; iProj++){
-      //measurements
-      for(int iHist=1; iHist<5; iHist++){
-        //if(iHist==2) continue;
-        fHiststd = (TH1D*) std_file.Get(Form("fHist%s_%s_%s_cut_%.1f_%c",lProjMeas[iHist],lVarName[0],lProj[iProj],kCuts[0][0][0],lAM[iMatter]));
-        fHistkf = (TH1D*) kf_file.Get(Form("fHist%s_%s_%s_cut_%.1f_%c",lProjMeas[iHist],lVarName[0],lProj[iProj],kCuts[0][0][0],lAM[iMatter]));
-        if(lMeasure[iHist-1]=="Efficiency"){
-          fHiststd->GetYaxis()->SetRange(0.,1.);
-          fHistkf->GetYaxis()->SetRange(0.,1.);
+      for(int iMeas=0; iMeas<3; iMeas++){
+        for(int iFile=0; iFile<3; iFile++){
+          fHist[iMeas][iFile] = (TH1D*) input_file[iFile]->Get(Form("NsigmaTPC/%s/%s/fHist%s_NsigmaTPC_%s_cut_%.1f_%c",lMeasure[iMeas],lProj[iProj],lProjMeas[iMeas],lProj[iProj],kCuts[0][0][0],lAM[iMat]));
+          set_style(fHist[iMeas][iFile],iFile);
+          fHist[iMeas][iFile]->SetTitle(lTitle[iFile]);
+          if(iMeas==0)
+            fHist[iMeas][iFile]->GetYaxis()->SetRange(0,1);
         }
-        fHiststd->SetTitle("std vertexer");
-        fHistkf->SetTitle("kf vertexer");
-        cv.cd();
-        fHistkf->Draw("PMC PLC");
-        fHiststd->Draw("SAME PMC PLC");
-        cv.SetName(Form("Comparison_%s_%s_%c",lMeasure[iHist-1],lProj[iProj],lAM[iMatter])); 
-        cv.BuildLegend();
-        if(lMeasure[iHist-1]=="Resolution"){
-          float max1=fHiststd->GetBinContent(fHiststd->GetMaximumBin());
-          float max2=fHistkf->GetBinContent(fHistkf->GetMaximumBin());
-          zeroline.SetY2(TMath::Max(max1+0.1,max2+0.1));
-          zeroline.SetLineColor(kRed);
-          zeroline.SetLineStyle(2);
-          zeroline.Draw("SAME");
-        }        
-        
-        cv.SaveAs(Form("comparison_images/comparison_%s_%s_%c.pdf",lMeasure[iHist-1],lProj[iProj],lAM[iMatter])); 
-
+        //set_range(fHist[iMeas]);
+        rainbow_plot(fHist[iMeas],cv,Form("RainbowPlot_%s_%s_%c",lMeasure[iMeas],lProj[iProj],lAM[iMat]),true);
         output_file.cd();
-        cv.Write();
-      }  
+        cv.Write();          
+        cv.Print(Form("%s",pdf_file)); 
+        cv.Clear();
+      }
+      multirainbow_plot(fHist,cv,Form("MultiRainbowPlot_%s_%c",lProj[iProj],lAM[iMat]),true);
+      cv.Write();          
+      cv.Print(Form("%s",pdf_file)); 
+      cv.Clear();    
     }
   }
+  cv.Print(Form("%s]",pdf_file));
 }
-*/
+
+void compare_resolutions(char* Std_name="resolutionStd.root", char* KF_name="resolutionKF.root", char* O2_name="resolution02.root", char* output_name="resolution_comparison.root", char* pdf_file="resolution_comparison.pdf", char* folder_name="nome")
+{
+  gStyle->SetOptStat(0);
+  gStyle->SetOptTitle(1);
+  
+  gSystem->Exec(Form("mkdir %s",folder_name));
+
+  const char lAM[3]{"AM"};
+  const char *lProj[2]{"pT", "ct"};
+  const char *lMeasure[3]={"Efficiency","FakeRate","CloneRate"};
+  const char* lProjMeas[3] = {"Rec","Fake","Clones"};
+  const char* lTitle[3] = {"Standard vertexer","0^{2} vertexer","Kalman Filter"};
+
+  TFile* input_file[3];
+  input_file[0] = new TFile(Std_name);
+  input_file[1] = new TFile(O2_name);
+  input_file[2] = new TFile(KF_name);
+  TFile output_file(output_name,"RECREATE");
+  TH1D* fHist[3][3];
+
+  TCanvas cv;
+  cv.Print(Form("%s[",pdf_file));
+  for(int iMat=0; iMat<2; iMat++){
+    for(int iProj=0; iProj<2; iProj++){
+      for(int iMeas=0; iMeas<3; iMeas++){
+        for(int iFile=0; iFile<3; iFile++){
+          fHist[iMeas][iFile] = (TH1D*) input_file[iFile]->Get(Form("NsigmaTPC/%s/%s/fHist%s_NsigmaTPC_%s_cut_%.1f_%c",lMeasure[iMeas],lProj[iProj],lProjMeas[iMeas],lProj[iProj],kCuts[0][0][0],lAM[iMat]));
+          set_style(fHist[iMeas][iFile],iFile);
+          fHist[iMeas][iFile]->SetTitle(lTitle[iFile]);
+          if(iMeas==0)
+            fHist[iMeas][iFile]->GetYaxis()->SetRange(0,1);
+        }
+        //set_range(fHist[iMeas]);
+        rainbow_plot(fHist[iMeas],cv,Form("RainbowPlot_%s_%s_%c",lMeasure[iMeas],lProj[iProj],lAM[iMat]),true);
+        output_file.cd();
+        cv.Write();          
+        cv.Print(Form("%s",pdf_file)); 
+        cv.Clear();
+      }
+      multirainbow_plot(fHist,cv,Form("MultiRainbowPlot_%s_%c",lProj[iProj],lAM[iMat]),true);
+      cv.Write();          
+      cv.Print(Form("%s",pdf_file)); 
+      cv.Clear();    
+    }
+  }
+  cv.Print(Form("%s]",pdf_file));
+}
+
+
+
 void ordered_indexes(int Var,int kCutsOrdered[]){
   float tmp;
   float OriginalOrder[number_of_variations];
