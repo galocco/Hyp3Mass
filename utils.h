336665cee63c8dcc77260683786d36207b33e0df
Giacomo Alocco <40990466+galocco@users.noreply.github.com>
2020-05-22 17:23:00 +0200

    Add files via upload

--- utils.h
+++ utils.h
@@ -12,14 +12,16 @@ void set_style(TH1D* hist, int color_index){
 }
 
 template <typename T,size_t size>
-void set_range(const T (&histos)[size], float err_mult=3){
+void set_range(const T (&histos)[size], float err_mult=5){
+  gStyle->SetPadRightMargin(0.1); 
+  gStyle->SetPadLeftMargin(0.15);
   float max,min;
   bool first=true;
   for(auto histo: histos){
     for(int bin=1; bin<=histo->GetNbinsX(); bin++){
       if(first){
-        max = histo->GetBinContent(bin)+histo->GetBinError(bin);
-        min = histo->GetBinContent(bin)-histo->GetBinError(bin);
+        max = histo->GetBinContent(bin)+histo->GetBinError(bin)*err_mult;
+        min = histo->GetBinContent(bin)-histo->GetBinError(bin)*err_mult;
         first=false;
       }
       if(histo->GetBinContent(bin)+histo->GetBinError(bin)*err_mult > max)
@@ -29,16 +31,20 @@ void set_range(const T (&histos)[size], float err_mult=3){
     }
   }
   
-  for(auto histo: histos)
-    histo->GetYaxis()->SetRange(min,max);
+  for(auto histo: histos){
+    histo->GetYaxis()->SetRangeUser(min,max);
+  }
 }
 
 template <typename T,size_t size>
 void rainbow_plot(const T (&histos)[size], TCanvas &cv, const char* canvas_name, bool legend=true, const char* plot_mode=""){
-  cv.SetWindowSize(600,400);
+  gStyle->SetPadRightMargin(0.1); 
+  gStyle->SetPadLeftMargin(0.15);
+  cv.SetWindowSize(800,450);
   cv.SetName(canvas_name);
   cv.cd();
   bool first=true;
+  //set_range(histos);
   for(auto histo: histos){
     if(first){
       first=false;
@@ -53,10 +59,39 @@ void rainbow_plot(const T (&histos)[size], TCanvas &cv, const char* canvas_name,
 
 template <typename T,size_t size_x,size_t size_y>
 void multirainbow_plot(const T (&histos)[size_x][size_y], TCanvas &multi_cv, const char* canvas_name, bool legend=true, const char* plot_mode=""){
-  multi_cv.SetWindowSize(600*size_x,400);  
+  gStyle->SetPadRightMargin(0.2); 
+  gStyle->SetPadLeftMargin(0.15);
+  multi_cv.SetWindowSize(800*size_x,450);  
   multi_cv.SetName(canvas_name);
-  TLegend *leg = new TLegend(0.2,0.2,0.5,0.5);
+  TLegend *leg=new TLegend(0.1,0.7,0.5,0.1);
   multi_cv.Divide(size_x,1);
+  bool first=true;
+  for(int x=0; x<size_x; x++){
+    for(auto histo: histos[x]){
+      multi_cv.cd(x+1);
+      if(first){
+        first=false;
+        histo->Draw(plot_mode);
+      }
+      else
+        histo->Draw(Form("SAME %s",plot_mode));
+      if(x==0)
+        leg->AddEntry(histo,histo->GetTitle(),"lp");
+    }
+  }
+
+  if(legend){
+    leg->SetTextFont(132);
+    multi_cv.cd(1);
+    leg->Draw();
+  }
+}
+template <typename T,size_t size_x,size_t size_y>
+void multirainbow_plotvert(const T (&histos)[size_x][size_y], TCanvas &multi_cv, const char* canvas_name, bool legend=true, const char* plot_mode=""){
+  multi_cv.SetWindowSize(800,450*size_x);  
+  multi_cv.SetName(canvas_name);
+  TLegend *leg=new TLegend(0.2,0.2,0.5,0.5);
+  multi_cv.Divide(1,size_x, 0.1, 0.1);
   bool first=true;
   for(int x=0; x<size_x; x++){
     for(auto histo: histos[x]){
@@ -103,4 +138,12 @@ void SetRateErrors(TH1D* HistRate, TH1D* HistGen){
       HistRate->SetBinContent(iBin,-1);
     }
   }
+}
+
+bool rejection(TF1* density_func,float variable, float max_value=1){
+  float prob = density_func->Eval(variable);
+  if(gRandom->Uniform(max_value)<prob)
+    return true;
+  else 
+    return false;
 }
